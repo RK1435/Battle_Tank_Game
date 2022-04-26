8e17c051cb0272bc3f481b73e63a40090af61323
RK1435 <bhatrajuravikiran@gmail.com>
2022-04-26 15:00:41 +0530

    MVC -- Feature3
    1. Implemented the MVC Architecture and created TankModel, TankView, TankController & TankSpawner
    2. Fixed the Cam position and made to follow the Player Tank

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -1,21 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-// In MVC there won't be any script is Singleton, rather than that MVC-S the S mean service which controlls total MVC will be a Singleton
-// This need to be Tank Controller
-// Tank View will be attached to the Game Object and Tank controller will be called in the Tank View
-// Like Tank View there will be Tank Model which will be Monobehaviour(as it should contain Unity Engine header coz it uses some services of it) but not attached to any Game Object 
-public class PlayerTankController: TankGenericSingleton<PlayerTankController>
+public class TankController
 {
-    public float mSpeed = 5.0f;
-    public float rSpeed = 200.0f;
-    public Joystick joystick;
-    void Update()
+    private TankModel tankModel;
+    private TankView tankView;
+    private Rigidbody rb;
+    public TankController(TankModel _tankModel, TankView _tankView)
     {
-        transform.Rotate(0, joystick.Horizontal * Time.deltaTime * rSpeed, 0);
-        transform.Translate(0, 0, joystick.Vertical * Time.deltaTime * mSpeed);
+        tankModel = _tankModel;
+        tankView = GameObject.Instantiate<TankView>(_tankView);
+        rb = tankView.GetRigidbody();
+
+        tankModel.SetTankController(this);
+        tankView.SetTankController(this);
     }
 
+    public void Move(float movement, float movementSpeed)
+    {
+        rb.velocity = tankView.transform.forward * movement * movementSpeed;
+    }
+
+    public void Rotate(float rotate, float rotateSpeed)
+    {
+        Vector3 vector = new Vector3(0f, rotate * rotateSpeed, 0f);
+        Quaternion deltaRotation = Quaternion.Euler(vector * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }    
+
+    public TankModel GetTankModel()
+    {
+        return tankModel;
+    }
 }
